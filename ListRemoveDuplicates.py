d56d289f9352cf0ec1d5d6851b2afa968bfbcd27
puziop93 <46887359+puziop93@users.noreply.github.com>
2019-09-14 18:56:50 +0200

    Create ListRemoveDuplicates.py

---
+++ ListRemoveDuplicates.py
@@ -0,0 +1,12 @@
+"""
+14) List Remove Duplicates
+Question
+Write a program (function!) that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.
+Solution
+"""
+def liste(x):
+   numbers = set(x)
+   new = list(numbers)
+   return print(new)
+y = [1,2,3,4,5,5,6,7,7,9]
+liste(y)
